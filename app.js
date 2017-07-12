d07ad5aff12128959a99ce6e0b2438efc8c0c373
Unknown <marc.payumo@gmail.com>
2017-07-12 14:54:06 -0700

    added a 'how many days' selector

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ weatherApp.config(function ($routeProvider) {
         templateUrl: 'pages/forecast.htm',
         controller: 'forecastController'
     })
+    
+    .when('/forecast/:days', {
+        templateUrl: 'pages/forecast.htm',
+        controller: 'forecastController'
+    })
 });
 
 // Services
@@ -24,7 +29,7 @@ weatherApp.service('cityService', function(){
 })
 // Controllers
 
-weatherApp.controller('homeController', ['$scope', '$resource', 'cityService', function ($scope, $resource, cityService){
+weatherApp.controller('homeController', ['$scope', '$resource', '$routeParams', 'cityService', function ($scope, $resource, $routeParams, cityService){
     $scope.city = cityService.city;
     
     $scope.$watch('city', function() {
@@ -32,14 +37,16 @@ weatherApp.controller('homeController', ['$scope', '$resource', 'cityService', f
     })
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', function ($scope, $resource, cityService){
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function ($scope, $resource, $routeParams, cityService){
     $scope.city = cityService.city;
+    
+    $scope.days = $routeParams.days || 2;
     
     $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=03ceb6e5f6fa839cb4e36d0685f15a6a", {
         callback: "JSON_CALLBACK" }, {get: { method: "JSONP" }}
     );
     
-    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2 });
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
     
     $scope.convertToCelsius = function(degK) {
         return Math.round(degK - 273.15);
