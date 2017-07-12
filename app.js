86b68b493d537fa28031de5b547396a96f105f65
Unknown <marc.payumo@gmail.com>
2017-07-12 14:38:32 -0700

    integrated API for weather

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ weatherApp.service('cityService', function(){
 })
 // Controllers
 
-weatherApp.controller('homeController', ['$scope', 'cityService', function ($scope, cityService){
+weatherApp.controller('homeController', ['$scope', '$resource', 'cityService', function ($scope, $resource, cityService){
     $scope.city = cityService.city;
     
     $scope.$watch('city', function() {
@@ -32,6 +32,14 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function ($sco
     })
 }]);
 
-weatherApp.controller('forecastController', ['$scope', 'cityService', function ($scope, cityService){
-    $scope.city = cityService.city;                     
+weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', function ($scope, $resource, cityService){
+    $scope.city = cityService.city;
+    
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=03ceb6e5f6fa839cb4e36d0685f15a6a", {
+        callback: "JSON_CALLBACK" }, {get: { method: "JSONP" }}
+    );
+    
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2 });
+    
+    
 }]);
