c8756785fbdf3a882a474369f8ea198200bd0585
Unknown <marc.payumo@gmail.com>
2017-07-18 09:45:16 -0700

    added links to 2, 5, 7 day forecast

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ weatherApp.controller('homeController', ['$scope', '$resource', '$routeParams', 
 weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function ($scope, $resource, $routeParams, cityService){
     $scope.city = cityService.city;
     
-    $scope.days = $routeParams.days || 2;
+    $scope.days = $routeParams.days || '2';
     
     $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=03ceb6e5f6fa839cb4e36d0685f15a6a", {
         callback: "JSON_CALLBACK" }, {get: { method: "JSONP" }}
