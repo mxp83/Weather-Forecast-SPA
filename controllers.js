59ac2f96da2f956d4d9bb7306ab88bc175feb0a4
Unknown <marc.payumo@gmail.com>
2017-07-18 10:11:53 -0700

    split each function into own js files

---
+++ controllers.js
@@ -0,0 +1,28 @@
+// Controllers
+
+weatherApp.controller('homeController', ['$scope', '$resource', '$routeParams', 'cityService', function ($scope, $resource, $routeParams, cityService){
+    $scope.city = cityService.city;
+    
+    $scope.$watch('city', function() {
+        cityService.city = $scope.city;
+    })
+}]);
+
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function ($scope, $resource, $routeParams, cityService){
+    $scope.city = cityService.city;
+    
+    $scope.days = $routeParams.days || '2';
+    
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=03ceb6e5f6fa839cb4e36d0685f15a6a", {
+        callback: "JSON_CALLBACK" }, {get: { method: "JSONP" }}
+    );
+    
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
+    
+    $scope.convertToCelsius = function(degK) {
+        return Math.round(degK - 273.15);
+    }
+    $scope.convertToDate = function(dt) {
+        return new Date(dt * 1000);
+    }
+}]);
